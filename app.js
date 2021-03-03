45d35deae01d906a635277c1e7aa867b17b31972
KingsMan04 <54734529+KingsMan04@users.noreply.github.com>
2021-03-03 11:57:04 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('Hello World')
+});
+
+app.listen(port, ()=>{
+    console.log(`Server running on http://localhost:${port}`);
+});
